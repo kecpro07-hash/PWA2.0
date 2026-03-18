f932a77c58e6e3b9cde2b36ce2a3398a5c9fffdd
kecpro07-hash <kecpro07@gmail.com>
2026-03-18 19:06:12 +0500

    Create config.js

---
+++ frontend/js/config.js
@@ -0,0 +1,21 @@
+// Конфигурация приложения
+const CONFIG = {
+    API_URL: window.location.origin,
+    ADMIN_ID: 'ваш_telegram_id', // Замените на свой Telegram ID
+    APP_NAME: 'У Нас Чисто',
+    APP_VERSION: '1.0.0',
+    
+    // Настройки бонусов
+    BONUS_PERCENT: 10,
+    BONUS_TO_RUB: 1,
+    MAX_BONUS_PERCENT: 50,
+    
+    // Настройки времени
+    MIN_PREP_TIME: 10,
+    TIME_INTERVAL: 10,
+    
+    // Платежи
+    SBP_PHONE: '8 (906 )801-67-38',
+    SBP_BANK: 'СберБанк',
+    SBP_NAME: 'Картавенко В. В.'
+};
